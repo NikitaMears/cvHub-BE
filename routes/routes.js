f1cf6543d934e7932a59665b6c0f357fa26408e5
Melaku Minas <minasmelaku@gmail.com>
2024-03-04 09:50:01 +0300

    removed commented code

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const userController = require('../controllers/userController');
-// const mobileSocialAuthController = require('../controllers/mobileSocialAuthController');
 const roleController = require('../controllers/roleController');
 const permissionController = require('../controllers/permissionController');
 const cvController = require('../controllers/cvController');
@@ -11,13 +10,9 @@ const projectController = require('../controllers/projectController');
 
 
 const { verifyToken } = require('../middlewares/verifyToken');
-// const clientController = require('../controllers/clientController');
-// const serviceController = require('../controllers/serviceController');
 
 
 const router = express.Router();
-
-// User routes
 router.post('/users', verifyToken, userController.createUser);
 router.get('/users', verifyToken, userController.getUsers);
 router.post('/users/change-password/:id',verifyToken, userController.changePassword);
@@ -70,29 +65,7 @@ router.put('/rfps/:id', rfpController.update);
 router.delete('/rfps/:id', rfpController.delete);
 router.post('/rfps/search', rfpController.search);
 
-// Routes for services
-// router.post('/services', serviceController.createService);
-// router.get('/services', serviceController.getAllServices);
-// router.get('/services/:id', serviceController.getServiceById);
-// router.put('/services/:id', serviceController.updateService);
-// router.delete('/services/:id', serviceController.deleteService);
-
-// // Routes for clients
-// router.post('/clients', clientController.createClient);
-// router.get('/clients', clientController.getAllClientsWithServices);
-// router.get('/clients/:id', clientController.getClientByIdWithServices);
-// router.put('/clients/:id', clientController.updateClient);
-// router.delete('/clients/:id', clientController.deleteClient);
-
-//Latest changes
-
-// router.post('/publicLogin', userController.publicLogin);
 router.post('/signup', userController.signup);
-// router.post('/clientLogin', clientController.clientLogin);
-// router.get('/clientProfile/:id', verifyToken, clientController.clientProfile);
-// router.put('/regenerateAPIKey/:id', verifyToken, clientController.regenerateAPIKey);
-// router.put('/cancelSubscription/:id', verifyToken, clientController.cancelServiceSubscription);
-// router.put('/changeClientPassword/:id', verifyToken, clientController.changeClientPassword);
 router.get('/dashboard',verifyToken, userController.dashboard);
 router.post('/signUpWithGmail', userController.signUpWithGmail);
 router.post('/signUpWithFacebook', userController.signUpWithFacebook);
@@ -100,8 +73,4 @@ router.post('/loginWithGmail', userController.loginWithGmail);
 router.post('/loginWithFacebook', userController.loginWithFacebook);
 router.post('/forgotPassword', userController.forgotPassword);
 router.post('/resetPassword', userController.resetPassword);
-// router.post('/mobile/signUpWithGmail', mobileSocialAuthController.signUpWithGmailMobile);
-// router.post('/mobile/signUpWithFacebook', mobileSocialAuthController.signUpWithFacebookMobile);
-// router.post('/mobile/loginWithGmail', mobileSocialAuthController.loginWithGmailMobile);
-// router.post('/mobile/loginWithFacebook', mobileSocialAuthController.loginWithFacebookMobile);
 module.exports = router;
